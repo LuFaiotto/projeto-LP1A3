247978b71da8ce25ec3f60d1e5ebe668eef7ef9e
Felipe <felipe-loureiro@outlook.com>
2019-04-30 11:42:36 -0300

    MapSET

--- src/br/edu/ifsp/spo/lp1a3/projeto/war/classes/Tabuleiro.java
+++ src/br/edu/ifsp/spo/lp1a3/projeto/war/classes/Tabuleiro.java
@@ -1,6 +1,12 @@
 package br.edu.ifsp.spo.lp1a3.projeto.war.classes;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Tabuleiro {
+
+	int diceSet;
+	ArrayList<Continente> continentes = new ArrayList<>();
 //Grafo dos territórios
 //Array List de continentes e um continente Array List de países e um país um Array List de conexões
 //String l = "Brasil; Argentina; Venezuela; Argélia";
@@ -10,8 +16,24 @@ public class Tabuleiro {
 //Método
 	//Análise da rolagem
 	//"Matar" o exército - Retorna peças do player, perdidas ou ganhas
+	public void loadGameConf(){
+		Map<Integer, Pais> mapa = new HashMap<Integer, Pais>();
+		continentes.add(new Continente("América Do Norte", new int [] {1,2,3,4,5,6,7,8,9}));
+		mapa.put(1, new Pais("Alaska", new int [] {2, 3, 37}));
+		mapa.put(2, new Pais("Mackenzie", new int [] {3, 4, 6}));
+		mapa.put(3, new Pais("Vancouver", new int [] {1, 2, 4}));
+		mapa.put(4, new Pais("Ottawa", new int [] {2, 3, 5, 7, 8}));
+		mapa.put(5, new Pais("Labrador", new int [] {4, 6, 8}));
+		mapa.put(6, new Pais("Groelândia", new int [] {2, 5, 21}));
+		mapa.put(7, new Pais("Califórnia", new int [] {3, 4, 8, 9}));
+		mapa.put(8, new Pais("Nova York", new int [] {4, 5, 7, 9}));
+		mapa.put(9, new Pais("México", new int [] {7, 8, 10}));
+		
+		continentes.add(new Continente("América do Sul", new int [] {10, 11, 12, 13}));
+		
+	}
+
 	
-	int diceSet; //Dice game set
 	
 //	public Tabuleiro() {
 //		
