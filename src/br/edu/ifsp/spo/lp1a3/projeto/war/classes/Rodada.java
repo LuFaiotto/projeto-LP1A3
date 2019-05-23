b83934c587821fae216fc3cd72e7e84f2c8c8700
Luana Tavares Faiotto <luana.faiotto@gmail.com>
2019-05-22 23:42:42 -0300

    Refatorando métodos de Exército e criando métodos-esqueletos

--- src/br/edu/ifsp/spo/lp1a3/projeto/war/classes/Rodada.java
+++ src/br/edu/ifsp/spo/lp1a3/projeto/war/classes/Rodada.java
@@ -5,27 +5,18 @@ public class Rodada {
 	private int ladosDado;
 	private Player player;
 	
-	public Rodada(int numLados, Player player) {
+	public Rodada(Player player, int numLados) {
 		setPlayer(player);
+		setLadosDado(numLados);
 	}
 	
 	public void iniciarRodada() {
 		
 	}
 	
-//	//Adicionar um ex�rcito a um pa�s que j� possui o ex�rcito base
-//		public void adicionarExercito(Player player, Pais pais, int qtdExercito) {
-//			//Valida se o ex�rcito base � o mesmo do player
-//			//Se sim:
-//			pais.setQtdExercito(pais.getQtdExercito() + qtdExercito);
-//			System.out.println(qtdExercito + " adicionados ao pa�s " + pais.getNome());
-//			//Sen�o:
-//			System.out.println("Voc� n�o pode adicionar ex�rcito em um pa�s de outro player.");
-//		}
-	
 	//TODO: Fazer a validação se o player tem permissão para fazer o movimento
 	public void adicionarExercito(Pais pais, int qtdExercito) {
-		//Validação se o exéricito base é o mesmo do player
+		//Validação se o exército base é o mesmo do player
 		//Se sim:
 		pais.setQtdExercito(pais.getQtdExercito() + qtdExercito);
 		System.out.println(qtdExercito + " adicionados ao pa�s " + pais.getNome());
@@ -33,18 +24,65 @@ public class Rodada {
 		System.out.println("Voc� n�o pode adicionar ex�rcito em um pa�s de outro player.");
 	}
 	
-//	//Método utilizado ao conquistar um território ou ao querer movimentar exércitos entre países
-//		public String movimentarExercito(Pais paisAtual, Pais paisFuturo, int qtdExercito) {
-//			if(validarConexao() && qtdExercito < paisAtual.getQtdExercito()) {
-//				paisAtual.setQtdExercito(paisAtual.getQtdExercito() - qtdExercito);
-//				paisFuturo.setQtdExercito(paisFuturo.getQtdExercito() + qtdExercito);
-//				return "Exército movimentado com sucesso";
-//			}
-//			return "Não é possível realizar essa movimentação";
-//		}
-	
 	public String movimentarExercito(Pais paisAtual, Pais paisFuturo, int qtdExercito) {
-		return "";
+		if(validarConexao() && qtdExercito < paisAtual.getQtdExercito()) {
+			paisAtual.setQtdExercito(paisAtual.getQtdExercito() - qtdExercito);
+			paisFuturo.setQtdExercito(paisFuturo.getQtdExercito() + qtdExercito);
+			return "Exército movimentado com sucesso";
+		}
+		return "Não é possível realizar essa movimentação";
+	}
+	
+	//TODO: Verificar validação do método getNumLados da classe Dado.
+	public void atacar(Pais atacante, Pais atacado, int qtdExercito) {
+		int valorRetornado;
+		boolean resultadoFinal = false;
+		
+		if(validarConexao() && validarAtaque()) {
+			for(int i = 0; i < qtdExercito; i++) {
+				valorRetornado = Tabuleiro.compareRolagemDados(getLadosDado());
+				if(valorRetornado == 1) {
+					atacado.setQtdExercito(atacado.getQtdExercito() - 1);
+					System.out.println("Vitória. Um exército oponente foi destruído.");
+				} else if (valorRetornado == 0){
+					atacante.setQtdExercito(atacante.getQtdExercito() - 1);
+					System.out.println("Empate. Um exército seu foi destruído.");
+				} else {
+					atacante.setQtdExercito(atacante.getQtdExercito() - 1);
+					System.out.println("Derrota. Um exército seu foi destruído.");
+				}
+				
+				//Valida se o atacado ainda possui exército
+				if(atacado.getQtdExercito() == 0) {
+					i = qtdExercito;
+					resultadoFinal = true;
+				}
+			}
+			
+			validarVencedor(resultadoFinal);
+		}
+	}
+	
+	//Método que valida se o atacante possui estrutura para cometer o ataque
+	//TODO: Implementar o método
+	//O que fazer: Avaliar se a quantidade de exércitos escolhidos está, no máximo, com a quantidade de
+	//exércitos posicionados no país de ataque - 1 (exército base)
+	public boolean validarAtaque() {
+		return false;
+	}
+	
+	//TODO: Implementar o método
+	public boolean validarConexao() {
+		return false;
+	}
+	
+	//TODO: Implementar o método
+	public void validarVencedor(boolean resultFinal) {
+		if(resultFinal) {
+			System.out.println("Vitória. Parabéns! Um exército seu será alocado no país conquistado.");
+		} else {
+			System.out.println("Derrota. O oponente defendeu o território.");
+		}
 	}
 
 	@SuppressWarnings("unused")
@@ -54,6 +92,14 @@ public class Rodada {
 
 	private void setPlayer(Player player) {
 		this.player = player;
+	}
+
+	private int getLadosDado() {
+		return ladosDado;
+	}
+
+	private void setLadosDado(int ladosDado) {
+		this.ladosDado = ladosDado;
 	}
 
 }
